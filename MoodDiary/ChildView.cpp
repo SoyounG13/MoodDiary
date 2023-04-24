67fc3a05657d3eb3de7a0b08335faf9ff71a6812
SoyounG13 <2870116@naver.com>
2023-04-25 03:33:00 +0900

    MoodDiary

---
+++ MoodDiary/ChildView.cpp
@@ -0,0 +1,159 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MoodDiary.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	em[mood].setNameSize(L"Mood", 6);
+	em[weather].setNameSize(L"Weather", 4);
+	em[emotion].setNameSize(L"Emotion", 11);
+	em[hobby].setNameSize(L"Hobby", 7);
+	em[food].setNameSize(L"Food", 7);
+	em[housework].setNameSize(L"Housework", 3);
+	camera.setPixel(450, 300);
+	save.setPixel(200, 150);
+
+	background.colorImg.Load(L"image/Background.png");
+	camera.colorImg.Load(L"image/Camera.png");
+	save.colorImg.Load(L"image/save.png");
+
+	for (int i = mood; i <= housework; i++)
+		em[i].Load();
+
+	b_moodSelect = false;
+	b_calendarMenu = false;
+	b_dailyCheckMenu = true;
+
+	dailyCheckMenu();
+}
+
+CChildView::~CChildView()
+{
+}
+
+void CChildView::calendarMenu()
+{
+	b_calendarMenu = true;
+	b_dailyCheckMenu = false;
+}
+
+void CChildView::dailyCheckMenu()
+{
+	b_calendarMenu = false;
+	b_dailyCheckMenu = true;
+}
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_TIMER()
+	ON_WM_CREATE()
+	ON_WM_ERASEBKGND()
+	ON_WM_LBUTTONUP()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+
+	if (b_calendarMenu)
+	{
+		return;
+	}
+
+	else if (b_dailyCheckMenu)
+	{
+		CPoint pointer;
+		pointer.x = 0;
+		pointer.y = 0;
+
+		background.draw(dc, pointer);
+
+		for (int i = mood; i <= hobby; i++)
+			pointer.y += em[i].draw(dc, pointer);
+
+		pointer.x = 450;
+		pointer.y = 0;
+
+		for (int i = food; i <= housework; i++)
+			pointer.y += em[i].draw(dc, pointer);
+
+		pointer.y += camera.draw(dc, pointer);
+
+		pointer.x = 575;
+
+		save.draw(dc, pointer);
+	}
+
+	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
+}
+
+
+
+void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	int xClick, yClick;
+	xClick = point.x;
+	yClick = point.y;
+
+	if (b_calendarMenu)
+	{
+		return;
+	}
+
+	else if (b_dailyCheckMenu)
+	{
+		if (em[mood].moodClick(point, b_moodSelect))
+			return;
+		for (int i = weather; i <= housework; i++)
+			if (em[i].click(point))
+				return;
+		if (camera.click(point))
+			return;
+		if (save.click(point))
+			return;
+	}
+
+	CWnd::OnLButtonDown(nFlags, point);
+}
+
+
+void CChildView::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	RedrawWindow();
+	CWnd::OnLButtonUp(nFlags, point);
+}
---
+++ MoodDiary/ChildView.h
@@ -0,0 +1,225 @@
+﻿
+// ChildView.h: CChildView 클래스의 인터페이스
+//
+
+
+#pragma once
+
+
+// CChildView 창
+typedef enum ElementName
+{
+	mood,
+	weather,
+	emotion,
+	hobby,
+	food,
+	housework
+};
+
+class Element
+{
+public:
+	CImage colorImg;
+	CImage grayImg;
+
+	int x;
+	int y;
+	int xPixel;
+	int yPixel;
+	int xPrintPixel;
+	int yPrintPixel;
+	bool select;
+
+	Element() { select = false; };
+
+	void setPixel(int xPixel, int yPixel)
+	{
+		this->xPixel = xPixel;
+		this->yPixel = yPixel;
+	}
+
+	void setPrintPixel(int xPrintPixel, int yPrintPixel)
+	{
+		this->xPrintPixel = xPrintPixel;
+		this->yPrintPixel = yPrintPixel;
+	}
+
+	bool checkClick(int x, int y)
+	{
+		if (xPrintPixel <= x && x <= xPrintPixel + xPixel)
+			if (yPrintPixel <= y && y <= yPrintPixel + yPixel)
+				return true;
+		return false;
+	}
+
+	void changeSelect()
+	{
+		select = !select;
+	}
+
+	int draw(CPaintDC& dc, CPoint point)
+	{
+		colorImg.Draw(dc, point);
+		setPrintPixel(point.x, point.y);
+
+		return yPixel;
+	}
+
+	bool click(CPoint point)
+	{
+		if (checkClick(point.x, point.y))
+		{
+			changeSelect();
+			return true;
+		}
+		return false;
+	}
+};
+
+class Elements
+{
+public:
+	Element title;
+	Element element[12]{};
+
+	CString name;
+
+	int size;
+
+	Elements() {};
+
+	void setNameSize(CString name, int size)
+	{
+		this->name = name;
+		this->size = size;
+	}
+
+	void Load()
+	{
+		CString dir;
+
+		dir.Format(L"image/%sTitle.png", name);
+		title.colorImg.Load(dir);
+		title.setPixel(200, 75);
+		for (int i = 0; i < size; i++)
+		{
+			dir.Format(L"image/%s/%s%d.png", name, name, i);
+			element[i].colorImg.Load(dir);
+			dir.Format(L"image/%sGray/%s%d_gray.png", name, name, i);
+			element[i].grayImg.Load(dir);
+			element[i].setPixel(90, 100);
+		}
+	}
+
+	int draw(CPaintDC& dc, CPoint point)
+	{
+		CPoint startPoint;
+		startPoint = point;
+
+		title.colorImg.Draw(dc, point.x, point.y);
+		point.y += title.yPixel;
+		for (int i = 0; i < size; i++)
+		{
+			if (element[i].select)
+				element[i].colorImg.Draw(dc, point.x, point.y);
+			else
+				element[i].grayImg.Draw(dc, point.x, point.y);
+
+			element[i].setPrintPixel(point.x, point.y);
+
+			point.x += element[i].xPixel;
+			if ((i + 1) % 5 == 0)
+			{
+				point.x = startPoint.x;
+				point.y += element[i].yPixel;
+			}
+		}
+
+		return point.y - startPoint.y + element[0].yPixel;
+	}
+
+	bool moodClick(CPoint point, bool& bms)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (bms)
+			{
+				if (element[i].checkClick(point.x, point.y) && element[i].select)
+				{
+					element[i].changeSelect();
+					bms = false;
+					return true;
+				}
+			}
+			else
+			{
+				if (element[i].checkClick(point.x, point.y))
+				{
+					element[i].changeSelect();
+					bms = true;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	bool click(CPoint point)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (element[i].checkClick(point.x, point.y))
+			{
+				element[i].changeSelect();
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+class CChildView : public CWnd
+{
+// 변수 생성
+	Element background;
+	Element camera;
+	Element save;
+
+	Elements em[6];
+	
+	bool b_moodSelect;
+	bool b_calendarMenu;
+	bool b_dailyCheckMenu;
+
+// 함수 생성
+	void calendarMenu();
+	void dailyCheckMenu();
+
+// 생성입니다.
+public:
+	CChildView();
+
+// 특성입니다.
+public:
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// 구현입니다.
+public:
+	virtual ~CChildView();
+
+	// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+};
+
