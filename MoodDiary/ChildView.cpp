322c679144b94f9a257fc4276f311959815d2e55
JunSpring <2870116@naver.com>
2023-06-11 00:17:00 +0900

    MoodDiary

--- MoodDiary/ChildView.cpp
+++ MoodDiary/ChildView.cpp
@@ -33,10 +33,24 @@ CChildView::CChildView()
 		em[i].Load();
 
 	b_moodSelect = false;
-	b_calendarMenu = false;
-	b_dailyCheckMenu = true;
 
-	dailyCheckMenu();
+	/* ----------------------------------------------- */
+	
+	ReadDataFromFile("data.txt");
+
+	month = 1;
+
+	left.setLeftRight(true);
+	right.setLeftRight(false);
+
+	background2.Img.Load(L"image/Background2.png");
+	me.Load();
+	left.Load();
+	right.Load();
+	de.Load();
+	fe.Load();
+
+	calendarMenu();
 }
 
 CChildView::~CChildView()
@@ -47,13 +61,261 @@ void CChildView::calendarMenu()
 {
 	b_calendarMenu = true;
 	b_dailyCheckMenu = false;
+
+	for(int i = 0; i < 6; i++)
+		em[mood].element[i].select = false;
+	b_moodSelect = false;
+	for (int i = 0; i < 4; i++)
+		em[weather].element[i].select = false;
+	for (int i = 0; i < 11; i++)
+		em[emotion].element[i].select = false;
+	for (int i = 0; i < 7; i++)
+		em[hobby].element[i].select = false;
+	for (int i = 0; i < 7; i++)
+		em[food].element[i].select = false;
+	for (int i = 0; i < 3; i++)
+		em[housework].element[i].select = false;
 }
 
 void CChildView::dailyCheckMenu()
 {
 	b_calendarMenu = false;
 	b_dailyCheckMenu = true;
+	
+	if (data[month - 1][date - 1].mood != 6)
+	{
+		em[mood].element[data[month - 1][date - 1].mood].select = true;
+		b_moodSelect = true;
+		for (int i = 0; i < 4; i++)
+			em[weather].element[i].select = data[month - 1][date - 1].weather[i];
+		for (int i = 0; i < 11; i++)
+			em[emotion].element[i].select = data[month - 1][date - 1].emotion[i];
+		for (int i = 0; i < 7; i++)
+			em[hobby].element[i].select = data[month - 1][date - 1].hobby[i];
+		for (int i = 0; i < 7; i++)
+			em[food].element[i].select = data[month - 1][date - 1].food[i];
+		for (int i = 0; i < 3; i++)
+			em[housework].element[i].select = data[month - 1][date - 1].housework[i];
+	}
 }
+
+void CChildView::ReadDataFromFile(string filename)
+{
+	const int MAX_MONTH = 12;
+	const int MAX_DATE = 31;
+
+	ifstream infile(filename);
+
+	string line;
+	while (getline(infile, line))
+	{
+		stringstream ss(line);
+		string token;
+
+		getline(ss, token, ' ');
+		int month = stoi(token);
+
+		getline(ss, token, ' ');
+		int date = stoi(token);
+
+		Data& d = data[month - 1][date - 1];
+
+		getline(ss, token, ' ');
+		d.mood = stoi(token);
+
+		for (int i = 0; i < 4; i++)
+		{
+			getline(ss, token, ' ');
+			d.housework[i] = stoi(token);
+		}
+
+		for (int i = 0; i < 11; i++)
+		{
+			getline(ss, token, ' ');
+			d.emotion[i] = stoi(token);
+		}
+
+		for (int i = 0; i < 7; i++)
+		{
+			getline(ss, token, ' ');
+			d.hobby[i] = stoi(token);
+		}
+		
+		for (int i = 0; i < 7; i++)
+		{
+			getline(ss, token, ' ');
+			d.food[i] = stoi(token);
+		}
+
+		for (int i = 0; i < 3; i++)
+		{
+			getline(ss, token, ' ');
+			d.housework[i] = stoi(token);
+		}
+
+		getline(ss, token, ' ');
+		d.picture = token;
+	}
+
+	infile.close();
+}
+
+// 웹캠으로부터 사진을 찍고 CImage에 저장하는 함수
+void CChildView::CaptureWebcamImage(CImage& image)
+{
+	VideoCapture capture(0);  // 웹캠을 엽니다.
+
+	if (!capture.isOpened())
+	{
+		AfxMessageBox(_T("웹캠을 열 수 없습니다."));
+		return;
+	}
+
+	Mat frame;
+	capture.read(frame);  // 웹캠으로부터 프레임을 읽어옵니다.
+
+	// 윈도우 생성 및 이미지 표시
+	cv::namedWindow("이미지");
+	cv::imshow("이미지", frame);
+
+	// 키 입력 대기
+	cv::waitKey(0);
+
+	// 윈도우 닫기
+	cv::destroyAllWindows();
+
+
+	// OpenCV의 Mat을 CImage로 변환합니다.
+	image.Create(frame.cols, frame.rows, 24);
+	uchar* imgBuffer = (uchar*)image.GetBits();
+	int imgPitch = image.GetPitch();
+
+	Mat imgRGB(frame.rows, frame.cols, CV_8UC3, imgBuffer, imgPitch);
+	cvtColor(frame, imgRGB, COLOR_BGR2RGB);
+
+	capture.release();  // 웹캠 사용이 끝났으므로 해제합니다.
+}
+
+// CImage를 PNG 파일로 저장하는 함수
+void CChildView::SaveImageToPNG(CImage& image, CString filename)
+{
+	// GDI+ 초기화
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+	// CImage를 GDI+ Bitmap으로 변환
+	Gdiplus::Bitmap bitmap((HBITMAP)image, NULL);
+
+	// 저장할 폴더 경로
+	CString folderPath;
+	SHGetFolderPath(NULL, CSIDL_MYPICTURES, NULL, 0, folderPath.GetBuffer(MAX_PATH));
+	folderPath.ReleaseBuffer();
+
+	// 저장할 파일 경로
+	CString filePath = folderPath + _T("\\picture\\") + filename;
+
+	// 폴더가 없는 경우 폴더를 생성합니다.
+	SHCreateDirectoryEx(NULL, folderPath + _T("\\picture"), NULL);
+
+	// PNG로 저장
+	bitmap.Save(filePath, &Gdiplus::ImageFormatPNG);
+
+	// GDI+ 종료
+	Gdiplus::GdiplusShutdown(gdiplusToken);
+}
+
+
+void CChildView::UpdateData(string filename, Data newData, int monthData, int dateData)
+{
+	ifstream infile(filename);
+	ofstream outfile("temp.txt");
+
+	string line;
+	bool foundDate = false;
+	while (getline(infile, line))
+	{
+		stringstream ss(line);
+		string token;
+
+		getline(ss, token, ' ');
+		int month = stoi(token);
+
+		getline(ss, token, ' ');
+		int date = stoi(token);
+
+		if (month == monthData && date == dateData)
+		{
+			// 이미 존재하는 날짜인 경우 정보를 수정
+			outfile << monthData << " " << dateData << " "
+				<< newData.mood << " ";
+			for (int i = 0; i < 4; i++)
+			{
+				outfile << newData.weather[i] << " ";
+			}
+			for (int i = 0; i < 11; i++)
+			{
+				outfile << newData.emotion[i] << " ";
+			}
+			for (int i = 0; i < 7; i++)
+			{
+				outfile << newData.hobby[i] << " ";
+			}
+			for (int i = 0; i < 7; i++)
+			{
+				outfile << newData.food[i] << " ";
+			}
+			for (int i = 0; i < 3; i++)
+			{
+				outfile << newData.housework[i] << " ";
+			}
+			outfile << endl;
+
+			foundDate = true;
+		}
+		else
+		{
+			// 다른 날짜는 그대로 유지
+			outfile << line << endl;
+		}
+	}
+
+	if (!foundDate)
+	{
+		// 새로운 날짜인 경우 추가
+		outfile << monthData << " " << dateData << " "
+			<< newData.mood << " ";
+		for (int i = 0; i < 4; i++)
+		{
+			outfile << newData.weather[i] << " ";
+		}
+		for (int i = 0; i < 11; i++)
+		{
+			outfile << newData.emotion[i] << " ";
+		}
+		for (int i = 0; i < 7; i++)
+		{
+			outfile << newData.hobby[i] << " ";
+		}
+		for (int i = 0; i < 7; i++)
+		{
+			outfile << newData.food[i] << " ";
+		}
+		for (int i = 0; i < 3; i++)
+		{
+			outfile << newData.housework[i] << " ";
+		}
+		outfile << endl;
+	}
+
+	infile.close();
+	outfile.close();
+
+	// 원본 파일 삭제 후 임시 파일을 원본 파일명으로 변경
+	remove(filename.c_str());
+	rename("temp.txt", filename.c_str());
+}
+
 
 BEGIN_MESSAGE_MAP(CChildView, CWnd)
 	ON_WM_PAINT()
@@ -89,7 +351,16 @@ void CChildView::OnPaint()
 
 	if (b_calendarMenu)
 	{
-		return;
+		CPoint pointer;
+		pointer.x = 0;
+		pointer.y = 0;
+
+		background2.draw(dc, pointer);
+		me.draw(dc, month);
+		left.draw(dc);
+		right.draw(dc);
+		de.draw(dc, month);
+		fe.draw(dc, month, data);
 	}
 
 	else if (b_dailyCheckMenu)
@@ -131,7 +402,17 @@ void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
 
 	if (b_calendarMenu)
 	{
-		return;
+		if (left.click(point, month))
+			return;
+		if (right.click(point, month))
+			return;
+		for (int i = 0; i < fe.size; i++)
+			if (fe.fe[i].click(point))
+			{
+				date = fe.fe[i].date;
+				dailyCheckMenu();
+				return;
+			}
 	}
 
 	else if (b_dailyCheckMenu)
@@ -144,7 +425,23 @@ void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
 		if (camera.click(point))
 			return;
 		if (save.click(point))
-			return;
+		{
+			for (int i = 0; i < 6; i++)
+				if (em[mood].element[i].select)
+					data[month - 1][date - 1].mood = i;
+			for (int i = 0; i < 4; i++)
+				data[month - 1][date - 1].weather[i] = em[weather].element[i].select;
+			for (int i = 0; i < 11; i++)
+				data[month - 1][date - 1].emotion[i] = em[emotion].element[i].select;
+			for (int i = 0; i < 7; i++)
+				data[month - 1][date - 1].hobby[i] = em[hobby].element[i].select;
+			for (int i = 0; i < 7; i++)
+				data[month - 1][date - 1].food[i] = em[food].element[i].select;
+			for (int i = 0; i < 3; i++)
+				data[month - 1][date - 1].housework[i] = em[housework].element[i].select;
+			UpdateData("data.txt", data[month - 1][date - 1], month, date);
+			calendarMenu();
+		}
 	}
 
 	CWnd::OnLButtonDown(nFlags, point);
--- MoodDiary/ChildView.h
+++ MoodDiary/ChildView.h
@@ -5,9 +5,32 @@
 
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <atlimage.h>
+#include <shlobj.h>
+#include <gdiplusheaders.h>
+using namespace std;
+using namespace cv;
 
 // CChildView 창
-typedef enum ElementName
+struct Data
+{
+	int mood = 6;
+
+	int weather[4];
+	int emotion[11];
+	int hobby[7];
+	int food[7];
+	int housework[3];
+
+	string picture;
+};
+
+enum ElementName
 {
 	mood,
 	weather,
@@ -23,8 +46,6 @@ public:
 	CImage colorImg;
 	CImage grayImg;
 
-	int x;
-	int y;
 	int xPixel;
 	int yPixel;
 	int xPrintPixel;
@@ -179,6 +200,341 @@ public:
 	}
 };
 
+class Element2
+{
+public:
+	CImage Img;
+
+	int xPixel;
+	int yPixel;
+	int xPrintPixel;
+	int yPrintPixel;
+
+	void setPixel(int xPixel, int yPixel)
+	{
+		this->xPixel = xPixel;
+		this->yPixel = yPixel;
+	}
+
+	void setPrintPixel(int xPrintPixel, int yPrintPixel)
+	{
+		this->xPrintPixel = xPrintPixel;
+		this->yPrintPixel = yPrintPixel;
+	}
+
+	void draw(CPaintDC& dc, CPoint point)
+	{
+		Img.Draw(dc, point);
+		setPrintPixel(point.x, point.y);
+	}
+};
+
+class MonthElement
+{
+public:
+	Element2 month[12];
+	Element2 letter;
+
+	void Load()
+	{
+		CString dir;
+
+		dir.Format(L"image/Month/month.png");
+		letter.Img.Load(dir);
+		letter.setPixel(50, 65);
+		for (int i = 1; i <= 12; i++)
+		{
+			dir.Format(L"image/Month/month%d.png", i);
+			month[i - 1].Img.Load(dir);
+			month[i - 1].setPixel(50, 65);
+		}
+	}
+
+	void draw(CPaintDC& dc, int num)
+	{
+		CPoint p(900 / 2 - 50, 0);
+		month[num - 1].draw(dc, p);
+		p.x = 900 / 2;
+		letter.draw(dc, p);
+	}
+};
+
+class MonthButtomElement : public Element2
+{
+public:
+	bool isLeft = false;
+	bool isRight = false;
+
+	void setLeftRight(bool isLeft)
+	{
+		if (isLeft)
+			this->isLeft = true;
+		else
+			this->isRight = true;
+	}
+
+	void Load()
+	{
+		CString dir;
+
+		if (isLeft)
+			dir.Format(L"image/Month/left.png");
+		else if (isRight)
+			dir.Format(L"image/Month/right.png");
+
+		Img.Load(dir);
+		setPixel(65, 65);
+	}
+
+	void draw(CPaintDC& dc)
+	{
+		CPoint point;
+
+		if (isLeft)
+			point = { 900 / 2 - 50 - 65, 0 };
+		else if (isRight)
+			point = { 900 / 2 + 50, 0 };
+
+		Img.Draw(dc, point);
+		setPrintPixel(point.x, point.y);
+	}
+
+	bool checkClick(int x, int y)
+	{
+		if (xPrintPixel <= x && x <= xPrintPixel + xPixel)
+			if (yPrintPixel <= y && y <= yPrintPixel + yPixel)
+				return true;
+		return false;
+	}
+
+	bool click(CPoint point, int& month)
+	{
+		if (checkClick(point.x, point.y))
+		{
+			if (isLeft)
+			{
+				if (month > 1)
+					month--;
+			}
+			else if (isRight)
+			{
+				if (month < 12)
+					month++;
+			}
+			return true;
+		}
+		return false;
+	}
+};
+
+class DateElement
+{
+public:
+	Element2 date[31];
+
+	void Load()
+	{
+		CString dir;
+
+		for (int i = 1; i <= 31; i++)
+		{
+			dir.Format(L"image/Date/date%d.png", i);
+			date[i - 1].Img.Load(dir);
+			date[i - 1].setPixel(128, 22);
+		}
+	}
+
+	void draw(CPaintDC& dc, int num)
+	{
+		int size;
+		switch (num)
+		{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			size = 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			size = 30;
+			break;
+		case 2:
+			size = 28;
+			break;
+		}
+
+		CPoint point;
+		switch (num)
+		{
+		case 1:
+		case 10:
+			point = { 6, 0 };
+			break;
+		case 2:
+		case 3:
+		case 11:
+			point = { 2, 0 };
+			break;
+		case 4:
+		case 7:
+			point = { 5, 0 };
+			break;
+		case 6:
+			point = { 3, 0 };
+			break;
+		case 5:
+		case 8:
+			point = { 1, 0 };
+			break;
+		case 9:
+		case 12:
+			point = { 4, 0 };
+			break;
+		}
+		point.x = point.x * 128 + 2;
+		point.y = point.y * 150 + 65 + 128;
+
+		for (int i = 1; i <= size; i++)
+		{
+			date[i - 1].draw(dc, point);
+
+			point.x += date[i - 1].xPixel;
+			if (point.x == 898)
+			{
+				point.x = 2;
+				point.y += 150;
+			}
+		}
+	}
+};
+
+class FlowerElement : public Element2
+{
+public:
+	int num;
+	int date;
+
+	bool checkClick(int x, int y)
+	{
+		if (xPrintPixel <= x && x <= xPrintPixel + xPixel)
+			if (yPrintPixel <= y && y <= yPrintPixel + yPixel)
+				return true;
+		return false;
+	}
+
+	bool click(CPoint point)
+	{
+		if (checkClick(point.x, point.y))
+		{
+			
+			return true;
+		}
+		return false;
+	}
+};
+
+class FlowerElements
+{
+public:
+	FlowerElement fe[31];
+	CImage Img[7];
+	int size;
+
+	void Load()
+	{
+		CString dir;
+
+		for (int i = 0; i <= 6; i++)
+		{
+			dir.Format(L"image/Flower/flower%d.png", i);
+			Img[i].Load(dir);
+		}
+
+		for (int i = 0; i < 31; i++)
+		{
+			fe[i].setPixel(128, 128);
+			fe[i].date = i + 1;
+		}
+	}
+
+	void draw(CPaintDC& dc, int num, Data data[12][31])
+	{
+		switch (num)
+		{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			size = 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			size = 30;
+			break;
+		case 2:
+			size = 28;
+			break;
+		}
+
+		CPoint point;
+		switch (num)
+		{
+		case 1:
+		case 10:
+			point = { 6, 0 };
+			break;
+		case 2:
+		case 3:
+		case 11:
+			point = { 2, 0 };
+			break;
+		case 4:
+		case 7:
+			point = { 5, 0 };
+			break;
+		case 6:
+			point = { 3, 0 };
+			break;
+		case 5:
+		case 8:
+			point = { 1, 0 };
+			break;
+		case 9:
+		case 12:
+			point = { 4, 0 };
+			break;
+		}
+		point.x = point.x * 128 + 2;
+		point.y = point.y * 150 + 65;
+
+		for (int i = 1; i <= size; i++)
+		{
+			fe[i - 1].num = data[num - 1][i - 1].mood;
+			
+			Img[fe[i - 1].num].Draw(dc, point);
+			fe[i - 1].setPrintPixel(point.x, point.y);
+
+			point.x += 128;
+			if (point.x == 898)
+			{
+				point.x = 2;
+				point.y += 150;
+			}
+		}
+	}
+};
+
 class CChildView : public CWnd
 {
 // 변수 생성
@@ -189,12 +545,33 @@ class CChildView : public CWnd
 	Elements em[6];
 	
 	bool b_moodSelect;
+
+	/* ------------------- */
+
+	Element2 background2;
+	MonthElement me;
+	MonthButtomElement left;
+	MonthButtomElement right;
+	DateElement de;
+	FlowerElements fe;
+
+	Data data[12][31];
+
+	int month;
+	int date;
+
+	/* ------------------- */
+
 	bool b_calendarMenu;
 	bool b_dailyCheckMenu;
 
 // 함수 생성
 	void calendarMenu();
 	void dailyCheckMenu();
+	void ReadDataFromFile(string filename);
+	void UpdateData(string filename, Data newData, int monthData, int dateData);
+	void CaptureWebcamImage(CImage& image);
+	void SaveImageToPNG(CImage& image, CString filename);
 
 // 생성입니다.
 public:
